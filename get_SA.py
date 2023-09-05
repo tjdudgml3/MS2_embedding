486bbb5d4c1cf6fe1d77150cd95e49fca1a16fd8
workplace <07seoy@naver.com>
2023-09-05 12:55:30 +0900

    analysis notebook for modification grouping

--- get_SA.py
+++ get_SA.py
@@ -163,11 +163,14 @@ def get_modified_SA(spec1 : list, spec2 : list, modified_mass, charge=1)->float:
 # grpup 1 unmodified seqeunce 끼리, group2 unmodified sequence끼리
 # 그 이후에 SA들의 평균값을 취한뒤에 SA_mean_group1/2 컬럼 두개를 생성
 
-all_mgf = []
-with open("./MS2_embedding/data/SA/mgf.pkl", "rb") as f:
-    all_mgf = pickle.load(f)
-print(len(all_mgf[0][0]))
-print(len(all_mgf))
+
+# all_mgf = []
+# with open("./MS2_embedding/data/SA/mgf.pkl", "rb") as f:
+#     all_mgf = pickle.load(f)
+# print(len(all_mgf[0][0]))
+# print(len(all_mgf))
+
+
 # exit()
 # path = "./MS2_embedding/hek293_mgf/"
 # file_list = os.listdir(path)
@@ -196,7 +199,7 @@ print(len(all_mgf))
 #     pickle.dump(all_mgf, f)
 # exit()
 # exit()
-def get_spectrum(spec1_path, spec1_idx):
+def get_spectrum(spec1_path, spec1_idx, mgf):
     spec1_path = spec1_path.split("\/")[-1]
     spec1_path = spec1_path.split("_")[4]
     path_int , path_letter = spec1_path[:2], spec1_path[2]
@@ -206,7 +209,7 @@ def get_spectrum(spec1_path, spec1_idx):
     else:
         path_int = int(path_int) - 1
     
-    spec1_mgf, index_begin, index_end = all_mgf[path_int]
+    spec1_mgf, index_begin, index_end = mgf[path_int]
     spec1_mgf = spec1_mgf[index_begin[spec1_idx-1]:index_end[spec1_idx-1]]
         
     spec1_list = []
@@ -215,13 +218,23 @@ def get_spectrum(spec1_path, spec1_idx):
         spec1_list.append(list(map(float,a.split(" "))))
         
     return spec1_list
-
+  
 def get_pair_wise_SA():
-    df = pd.read_csv("./MS2_embedding/sorted.tsv", delimiter="\t")
+    df = pd.read_csv("annotation_processed_input.csv")
+    # print(df.shape)
+
     # df = df[:20000]
+    # df = df.loc[df["Probability"] > 0.9, :]
+    # print(df.shape)
+    # df.to_csv("start_with_prob_filtered.csv", index=False)
+    # exit()
     start_idx = 0
     prev_seq = df["seq_stripped"][0]
     result = []
+    modification_list = []
+    temp_list_for_unmod = []
+    temp_list_for_mod = {}
+    
     for i,(cur_seq, spec_path, file_idx, idx) in enumerate(zip(df["seq_stripped"],df["SpectrumFile"], df["Index"], df["idx"])):
         end_idx = i
         # print(prev_seq, cur_seq)
@@ -234,22 +247,24 @@ def get_pair_wise_SA():
             SA_rand_list = []
             dic = {}
             mod = 0
+            
             for query_idx in range(start_idx, end_idx):
                 #쿼리가 unmodified라면
                 SA_mean = -1
                 SA_mean2 = -1
                 SA_mean3 = -1
                 SA_mean4 = -1            
-                if df.loc[query_idx]["ModificationAnnotation"] is np.nan:
+                if df.loc[query_idx]["ModificationAnnotation"] == '-1':
                     for cal_idx in range(start_idx, end_idx):
-                        if df.loc[cal_idx]["ModificationAnnotation"] is np.nan:
+                        if df.loc[cal_idx]["ModificationAnnotation"] == '-1':
                             
                             if cal_idx == query_idx:
                                 continue
                             
-                            spec1 = get_spectrum(df.loc[query_idx]["SpectrumFile"], df.loc[query_idx]["Index"])
-                            spec2 = get_spectrum(df.loc[cal_idx]["SpectrumFile"], df.loc[cal_idx]["Index"])
+                            spec1 = get_spectrum(df.loc[query_idx]["SpectrumFile"], df.loc[query_idx]["Index"],all_mgf)
+                            spec2 = get_spectrum(df.loc[cal_idx]["SpectrumFile"], df.loc[cal_idx]["Index"],all_mgf)
                             SA_list.append(get_SA(spec1, spec2))
+                            temp_list_for_unmod.append(get_SA(spec1, spec2))
                     if SA_list:
                         SA_mean = max(SA_list)
                     else:
@@ -260,23 +275,29 @@ def get_pair_wise_SA():
                 else:
                     for cal_idx in range(start_idx, end_idx):
                         #cal peptide 가 modified가 아닐때 : mod - umod 비교
-                        if df.loc[cal_idx]["ModificationAnnotation"] is np.nan:
+                        if df.loc[cal_idx]["ModificationAnnotation"] == '-1':
                             if cal_idx == query_idx:
                                 continue
                             
                             # if i > 600:
                             #     print(df.loc[query_idx]["Peptide"])
                             #     print(df.loc[cal_idx]["Peptide"])
-                            spec1 = get_spectrum(df.loc[query_idx]["SpectrumFile"], df.loc[query_idx]["Index"])
-                            spec2 = get_spectrum(df.loc[cal_idx]["SpectrumFile"], df.loc[cal_idx]["Index"])
-                            # print("---------------------start")        
-                            if "C+" in df.loc[query_idx]["Peptide"]:
-                                if get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"]-57.021) >get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"]):
-                                    SA_mod_list.append(get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"]-57.021))
-                                else:
-                                    SA_mod_list.append(get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"]))
+                            spec1 = get_spectrum(df.loc[query_idx]["SpectrumFile"], df.loc[query_idx]["Index"],all_mgf)
+                            spec2 = get_spectrum(df.loc[cal_idx]["SpectrumFile"], df.loc[cal_idx]["Index"],all_mgf)
+                            # print("---------------------start")
+                            delta_mass = df.loc[query_idx]["CalculatedMW"] - df.loc[cal_idx]["CalculatedMW"]        
+                            # if "C+" in df.loc[query_idx]["Peptide"]:
+                            #     if get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"]-57.021) >get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"]):
+                            #         SA_mod_list.append(get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"]-57.021))
+                            #     else:
+                            #         SA_mod_list.append(get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"]))
+                            # else:
+                            if df.loc[query_idx]["mod_processed"] not in temp_list_for_mod:
+                                temp_list_for_mod[df.loc[query_idx]["mod_processed"]] = [get_modified_SA(spec1, spec2, delta_mass)]
                             else:
-                                SA_mod_list.append(get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"])) #
+                                temp_list_for_mod[df.loc[query_idx]["mod_processed"]].append(get_modified_SA(spec1, spec2, delta_mass))
+                            # temp_list_for_mod.append((get_modified_SA(spec1, spec2, delta_mass), df.loc[query_idx]["ModificationAnnotation"]))
+                            SA_mod_list.append(get_modified_SA(spec1, spec2, delta_mass)) #
                             # SA_list.append(df.loc[query_idx]["Peptide"])
                             SA_list.append(get_SA(spec1, spec2))
                             # print("---------------------end")
@@ -285,12 +306,13 @@ def get_pair_wise_SA():
                             if cal_idx == query_idx:
                                 continue
                             if df.loc[query_idx]["Peptide"] == df.loc[cal_idx]["Peptide"]:
-                                spec1 = get_spectrum(df.loc[query_idx]["SpectrumFile"], df.loc[query_idx]["Index"])
-                                spec2 = get_spectrum(df.loc[cal_idx]["SpectrumFile"], df.loc[cal_idx]["Index"])
+                                delta_mass = df.loc[query_idx]["CalculatedMW"] - df.loc[cal_idx]["CalculatedMW"]
+                                spec1 = get_spectrum(df.loc[query_idx]["SpectrumFile"], df.loc[query_idx]["Index"],all_mgf)
+                                spec2 = get_spectrum(df.loc[cal_idx]["SpectrumFile"], df.loc[cal_idx]["Index"],all_mgf)
                                 # if df.loc[query_idx]["ModificationAnnotation"] is not np.nan:
                                     
                                 # print("---------------------start with mod")
-                                SA_same_mod_list_mod.append(get_modified_SA(spec1, spec2, df.loc[query_idx]["modified_mass"])) #
+                                SA_same_mod_list_mod.append(get_modified_SA(spec1, spec2, delta_mass)) #
                                 # SA_list.append(df.loc[query_idx]["Peptide"])
                                 SA_same_mod_list.append(get_SA(spec1, spec2)) 
                                 # print("---------------------end with mod")
@@ -323,25 +345,35 @@ def get_pair_wise_SA():
                 df_new = df.loc[abs(df["ObservedMW"] - query_mass) < 350,["idx"]]
                 rand_idx = df_new.sample(n=1)["idx"].item()
                 
-                spec1 = get_spectrum(df.loc[query_idx]["SpectrumFile"], df.loc[query_idx]["Index"])
-                series= df.loc[df["idx"] == rand_idx,["SpectrumFile", "Index"]]
+                spec1 = get_spectrum(df.loc[query_idx]["SpectrumFile"], df.loc[query_idx]["Index"],all_mgf)
+                series= df.loc[df["idx"] == rand_idx,["SpectrumFile", "Index", "CalculatedMW"]]
                 spec2_file = series["SpectrumFile"].item()
                 spec2_idx = series["Index"].item()
                 
-                spec2 = get_spectrum(spec2_file, spec2_idx)
-                SA_val = get_SA(spec1, spec2)
-                result.append((mod,SA_mean, SA_mean2, SA_mean3, SA_mean4, df.loc[query_idx]["idx"], 2, SA_val, rand_idx,start_idx)) 
-                
+                spec2 = get_spectrum(spec2_file, spec2_idx,all_mgf)
+                # SA_val = get_SA(spec1, spec2)
+                SA_val = get_modified_SA(spec1, spec2, df.loc[query_idx]["CalculatedMW"] - series["CalculatedMW"].item())
+                result.append((mod,SA_mean, SA_mean2, SA_mean3, SA_mean4, df.loc[query_idx]["idx"], 2, SA_val, rand_idx,start_idx,df.loc[query_idx]["ModificationAnnotation"], df.loc[query_idx]["SpectrumFile"], df.loc[query_idx]["Index"], df.loc[query_idx]["seq_stripped"],df.loc[query_idx]["CalculatedMW"])) 
+            
+            for mod_annotation in temp_list_for_mod:
+                if temp_list_for_unmod:
+                    modification_list.append((df.loc[start_idx]["seq_stripped"], mod_annotation, max(temp_list_for_unmod), max(temp_list_for_mod[mod_annotation])))
             prev_seq = cur_seq
             start_idx = i
+            temp_list_for_unmod = []
+            temp_list_for_mod = {}
         # if len(result) > 5:
         #     print(i, result[-4:])
         if i % 1000 == 0:
             print(i)
+    with open("modification_class","w") as f:
+        f.write("seq_stripped,Annotation,unmod-unmod,mod-unmod\n")
+        for a in modification_list:
+            f.write(f"{a[0]},{a[1]},{a[2]},{a[3]}\n")
     with open("SA_mean.txt", "w") as f:
-        f.write("mod,SA,SA_mod,SA_same_mod,SA_same_mod_with_mod,idx,rand,rand_SA,rand_idx,start_idx\n")
+        f.write("mod,SA,SA_mod,SA_same_mod,SA_same_mod_with_mod,idx,rand,rand_SA,rand_idx,start_idx,ModificationAnnotation,SpectrumFile,Index,seq_stripped,CalculatedMW\n")
         for i,a in enumerate(result):
-            f.write(f"{a[0]},{a[1]},{a[2]},{a[3]},{a[4]},{a[5]},{a[6]},{a[7]},{a[8]},{a[9]}\n")
+            f.write(f"{a[0]},{a[1]},{a[2]},{a[3]},{a[4]},{a[5]},{a[6]},{a[7]},{a[8]},{a[9]},{a[10]},{a[11]},{a[12]},{a[13]},{a[14]}\n")
                    
         
-get_pair_wise_SA()
+# get_pair_wise_SA()
